1af713cee7a840a0335eac3731b5ba6a6f576bd9
san12 <spencenewcomb08@gmail.com>
2020-08-24 19:06:12 -0600

    add data files

---
+++ sams/R/data.R
@@ -0,0 +1,53 @@
+#' Multivariate Independent Bernoulli Data (p = 6)
+#'
+#' Multivariate categorical data, generated as five clusters of independent variables with different
+#'   success probabilities.
+#'
+#' @format A data frame with 100 rows and 6 variables
+#'
+"p6_bernoulli"
+
+#' Multivariate Independent Bernoulli Data (p = 18)
+#'
+#' Multivariate categorical data, generated as five clusters of independent variables with different
+#'   success probabilities.
+#'
+#' @format A data frame with 100 rows and 18 variables.
+#'
+"p18_bernoulli"
+
+#' Large Sample Multivariate Independent Bernoulli Data (p = 6)
+#'
+#' Multivariate categorical data, generated as five clusters of independent variables with different
+#'   success probabilities.
+#'
+#' @format A data frame with 1000 rows and 6 variables.
+#'
+"p6_big_bernoulli"
+
+#' Independent Multivariate Normal Data (p = 6)
+#'
+#' Multivariate continuous data, generated as five clusters of independent variables with different
+#'   means.
+#'
+#' @format A data frame with 100 rows and 6 variables.
+#'
+"p6_mvn"
+
+#' Independent Multivariate Normal Data (p = 18)
+#'
+#' Multivariate continuous data, generated as five clusters of independent variables with different
+#'   means.
+#'
+#' @format A data frame with 100 rows and 18 variables
+#'
+"p18_mvn"
+
+#' Correlated Multivariate Normal Data (p = 18)
+#'
+#' Multivariate continuous data, generated as five clusters with an AR(1) correlation structure
+#'   determining the common covariance matrix for each observation.
+#'
+#' @format A data frame with 100 rows and 18 variables
+#'
+"p18_corr_mvn"
